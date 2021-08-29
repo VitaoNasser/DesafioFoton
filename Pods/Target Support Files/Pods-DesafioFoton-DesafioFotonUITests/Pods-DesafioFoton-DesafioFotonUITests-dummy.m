fcdd622d6efb9cc26be0a9473c12aaba330b2c6c
Rodrigo Dias <rodrigodias@MacBook-Pro-de-Rodrigo.local>
2021-08-28 23:01:08 -0300

    install cocopods

---
+++ Pods/Target Support Files/Pods-DesafioFoton-DesafioFotonUITests/Pods-DesafioFoton-DesafioFotonUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DesafioFoton_DesafioFotonUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DesafioFoton_DesafioFotonUITests
+@end
---
+++ Pods/Target Support Files/Pods-DesafioFotonTests/Pods-DesafioFotonTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DesafioFotonTests : NSObject
+@end
+@implementation PodsDummy_Pods_DesafioFotonTests
+@end
